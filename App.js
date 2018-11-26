fd48d282e57da75014de9426511e744b40ba5ed4
charian-laptop <charian@me.com>
2018-11-26 11:37:03 +0900

    키 삭제

--- App.js
+++ App.js
@@ -10,13 +10,6 @@ import {datagokr_KEY} from "./Keys"
 import {kakao_KEY} from "./Keys"
 import {token} from "./Keys"
 
-// API List
-// const API_KEY = "T4pjG2TFl6rGlAhzoYeo3bbLEpkmzeiF";
-// const datagokr_KEY = "aiMmkue91q4efKfTD0%2FfGF7Zv%2FIFyFZLqLVxcVrI7O9ZEgiKsA1yawel2kBvXgBvwaIAZ7ygUg%2BFJN1ex7n65w%3D%3D";
-// const kakao_KEY = "d114ff2a6ca1e7124eb497fbfcb660a4"
-// const token = "b8e396fd8d70334cc7860ccc70ae8ee2bbe42d07"
-
-
 
 export default class CustomDrawer extends Component {
 
